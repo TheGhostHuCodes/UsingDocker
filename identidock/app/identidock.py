cc59092f9955931bcfd2dd9366c11c6c8d1b180f
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-06-06 13:48:08 -0700

    Handle POST request to generate image from form variable.

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,22 +1,25 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import urllib.parse
 
 app = Flask(__name__)
 default_name = 'Joe Bloggs'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
     name = default_name
+    if request.method == 'POST':
+        name = request.form['name']
 
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
-           Hello <input type="text" name="name" value="{}">
+           Hello <input type="text" name="name" value="{0}">
            <input type="submit" value="submit">
            </form>
            <p>You look like a:
-           <img src="/moster/moster.png"/>
-           '''.format(name)
+           <img src="/moster/{1}"/>
+           '''.format(name, urllib.parse.quote(name))
     footer = '</body></html>'
 
     return header + body + footer
