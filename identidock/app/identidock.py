a2c442bfc53ab840805cde262ec28a53dd6368cc
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-01-31 09:31:52 -0800

    Hash user input before generating monster

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,10 +1,12 @@
 from flask import Flask, Response, request
 import requests
+import hashlib
 import urllib.parse
 import redis
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
+salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
 
@@ -14,6 +16,9 @@ def mainpage():
     if request.method == 'POST':
         name = request.form['name']
 
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
            Hello <input type="text" name="name" value="{0}">
@@ -21,7 +26,7 @@ def mainpage():
            </form>
            <p>You look like a:
            <img src="/monster/{1}"/>
-           '''.format(name, urllib.parse.quote(name))
+           '''.format(name, name_hash)
     footer = '</body></html>'
 
     return header + body + footer
